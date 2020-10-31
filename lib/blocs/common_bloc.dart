48868eea36fd569b35348193949eeede078e26d8
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-10-31 20:06:44 +0530

    Improved BLoC, Added more methods

--- lib/blocs/common_bloc.dart
+++ lib/blocs/common_bloc.dart
@@ -5,18 +5,17 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class CommonBloc {
-  StreamController<bool> _worldDataLoadingController =
-      StreamController<bool>.broadcast();
-  StreamController<bool> _countriesDataLoadingController =
-      StreamController<bool>();
-  StreamController<bool> _indiaDataLoadingController = StreamController<bool>();
+  final _worldDataLoadingController = StreamController<bool>.broadcast();
+  final _countriesDataLoadingController = StreamController<bool>();
+  final _indiaDataLoadingController = StreamController<bool>();
+  final _combinedDataLoadingController = StreamController<bool>.broadcast();
 
   Map worldData;
   List countryData;
   Map indiaData;
 
-  final snackBar = SnackBar(
-    content: const Text('Press back again to exit'),
+  final snackBar = const SnackBar(
+    content: Text('Press back again to exit'),
     duration: snackBarDuration,
   );
   DateTime backButtonPressedTime;
@@ -26,6 +25,7 @@ class CommonBloc {
   Stream<bool> get countriesDataLoadingStream =>
       _countriesDataLoadingController.stream;
   Stream<bool> get indiaDataLoadingStream => _indiaDataLoadingController.stream;
+  Stream<bool> get dataLoadingStream => _combinedDataLoadingController.stream;
 
   void setCountriesDataLoading(bool isLoading) =>
       _countriesDataLoadingController.add(isLoading);
@@ -33,17 +33,20 @@ class CommonBloc {
       _worldDataLoadingController.add(isLoading);
   void setIndiaDataLoading(bool isLoading) =>
       _indiaDataLoadingController.add(isLoading);
+  void setDataLoading(bool isLoading) =>
+      _combinedDataLoadingController.add(isLoading);
 
   void dispose() {
     _worldDataLoadingController.close();
     _countriesDataLoadingController.close();
     _indiaDataLoadingController.close();
+    _combinedDataLoadingController.close();
   }
 
   Future<bool> onWillPop(BuildContext context) async {
-    DateTime currentTime = DateTime.now();
+    final currentTime = DateTime.now();
 
-    bool backButtonNotPressedTwice = backButtonPressedTime == null ||
+    final backButtonNotPressedTwice = backButtonPressedTime == null ||
         currentTime.difference(backButtonPressedTime) > snackBarDuration;
 
     if (backButtonNotPressedTwice) {
@@ -55,7 +58,7 @@ class CommonBloc {
   }
 
   Future<void> getWorldWideData() async {
-    http.Response data = await http.get('https://corona.lmao.ninja/v2/all');
+    final data = await http.get('https://corona.lmao.ninja/v2/all');
     worldData = json.decode(data.body);
     setWorldDataLoading(false);
   }
@@ -68,15 +71,18 @@ class CommonBloc {
   }
 
   Future<void> getIndiaData() async {
-    http.Response data = await http.get(
+    final data = await http.get(
         'https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise');
     indiaData = json.decode(data.body);
     setIndiaDataLoading(false);
   }
 
-  Future<void> loadDataOnRefresh() async {
-    await getWorldWideData();
-    await getCountriesData();
+  Future<void> getCombinedData() async {
+    await Future.wait([
+      getWorldWideData(),
+      getCountriesData(),
+    ]);
+    setDataLoading(false);
   }
 
   Future<void> loadIndiaDataOnRefresh() async {
