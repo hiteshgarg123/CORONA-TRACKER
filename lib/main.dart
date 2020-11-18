7bfde862590d6885669ba61d2ad85c5def70253b
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-11-18 19:08:47 +0530

    Open, close and compact the hive boxes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,52 @@
 import 'package:covid_19_tracker/data/data.dart';
+import 'package:covid_19_tracker/data/hive_boxes.dart';
+import 'package:covid_19_tracker/models/countriesData.dart';
+import 'package:covid_19_tracker/models/indiaData.dart';
+import 'package:covid_19_tracker/models/worldData.dart';
 import 'package:covid_19_tracker/pages/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(WorldDataAdapter());
+  Hive.registerAdapter(CountriesDataAdapter());
+  Hive.registerAdapter(IndiaDataAdapter());
+  await Hive.openBox<WorldData>(HiveBoxes.worldData);
+  await Hive.openBox<CountriesData>(HiveBoxes.countriesData);
+  await Hive.openBox<IndiaData>(HiveBoxes.indiaData);
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void dispose() {
+    Hive.box<WorldData>(HiveBoxes.worldData).compact();
+    Hive.box<CountriesData>(HiveBoxes.countriesData).compact();
+    Hive.box<IndiaData>(HiveBoxes.indiaData).compact();
+    Hive.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    //TODO! REMOVE AFTER TEST
+
+    Box worldDataBox = Hive.box<WorldData>(HiveBoxes.worldData);
+    print('No. of Items in ${HiveBoxes.worldData} is ${worldDataBox.length}');
+    Box countriesDataBox = Hive.box<CountriesData>(HiveBoxes.countriesData);
+    print(
+        'No. of Items in ${HiveBoxes.countriesData} is ${countriesDataBox.length}');
+    Box indiaDataBox = Hive.box<IndiaData>(HiveBoxes.indiaData);
+    print('No. of Items in ${HiveBoxes.indiaData} is ${indiaDataBox.length}');
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'COVID-19 Tracker',
