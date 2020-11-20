52eec8149080d0dd3fa6c022f32c88878bcff6f2
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-11-20 20:04:15 +0530

    Hive boxes names

---
+++ lib/data/hive_boxes.dart
@@ -0,0 +1,5 @@
+class HiveBoxes {
+  static const String worldData = 'worldData_box';
+  static const String countriesData = 'countriesData_box';
+  static const String indiaData = 'indiaData_box';
+}
