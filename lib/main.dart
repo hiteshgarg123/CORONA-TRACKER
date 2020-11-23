23ca6a38223877fe2bea2b7a83addedaf1d49226
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-11-23 19:11:16 +0530

    Made CommonBloC as common ancesstor (parent of materialApp) and removed from top of different pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:covid_19_tracker/blocs/common_bloc.dart';
 import 'package:covid_19_tracker/data/data.dart';
 import 'package:covid_19_tracker/data/hive_boxes.dart';
 import 'package:covid_19_tracker/models/countryData.dart';
@@ -7,6 +8,7 @@ import 'package:covid_19_tracker/pages/homePage.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -27,14 +29,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'COVID-19 Tracker',
-      theme: ThemeData(
-        fontFamily: 'Circular',
-        primaryColor: primaryBlack,
+    return Provider<CommonBloc>(
+      create: (context) => CommonBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'COVID-19 Tracker',
+        theme: ThemeData(
+          fontFamily: 'Circular',
+          primaryColor: primaryBlack,
+        ),
+        home: HomePage(),
       ),
-      home: HomePage.create(context),
     );
   }
 
