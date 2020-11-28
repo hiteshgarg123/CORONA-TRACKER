142a722aec5db1e683a3ccd668838f6b5445642e
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-11-28 14:48:25 +0530

    Basic theming strategy and saving preferences

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -2,25 +2,75 @@ import 'package:covid_19_tracker/data/data.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final darkTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: primaryBlack,
-    brightness: Brightness.dark,
-    backgroundColor: const Color(0xFF212121),
-    accentColor: Colors.white,
-    accentIconTheme: IconThemeData(color: Colors.black),
-    dividerColor: Colors.black12,
-    fontFamily: 'Circular',
-  );
+  static ThemeData themeData(bool isDarkTheme) {
+    return isDarkTheme ? darkTheme() : lightTheme();
+  }
 
-  static final lightTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: primaryBlack,
-    brightness: Brightness.light,
-    backgroundColor: const Color(0xFFE5E5E5),
-    accentColor: Colors.black,
-    accentIconTheme: IconThemeData(color: Colors.white),
-    dividerColor: Colors.white54,
-    fontFamily: 'Circular',
-  );
+  static ThemeData lightTheme() {
+    return ThemeData(
+      primaryColor: primaryBlack,
+      fontFamily: 'Circular',
+      backgroundColor: Color(0xffF1F5FB),
+      indicatorColor: Color(0xffCBDCF8),
+      buttonColor: primaryBlack,
+      highlightColor: Color(0xffFCE192),
+      hoverColor: Color(0xff4285F4),
+      disabledColor: Colors.grey,
+      textSelectionColor: Colors.black,
+      cardColor: Colors.white,
+      canvasColor: Colors.grey[50],
+      brightness: Brightness.light,
+      textTheme: TextTheme(
+        headline1: TextStyle(
+          fontSize: 25,
+          color: primaryBlack,
+          fontWeight: FontWeight.bold,
+        ),
+        headline2: TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        headline3: TextStyle(
+          fontSize: 16,
+          color: primaryBlack,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  static ThemeData darkTheme() {
+    return ThemeData(
+      primaryColor: primaryBlack,
+      backgroundColor: primaryBlack,
+      fontFamily: 'Circular',
+      indicatorColor: Color(0xff0E1D36),
+      buttonColor: Colors.white70,
+      highlightColor: Color(0xff372901),
+      hoverColor: Color(0xff3A3A3B),
+      disabledColor: Colors.grey,
+      textSelectionColor: Colors.white,
+      cardColor: Color(0xFF151515),
+      canvasColor: Colors.black,
+      brightness: Brightness.dark,
+      textTheme: TextTheme(
+        headline1: TextStyle(
+          fontSize: 25,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        headline2: TextStyle(
+          fontSize: 20,
+          color: primaryBlack,
+          fontWeight: FontWeight.bold,
+        ),
+        headline3: TextStyle(
+          fontSize: 16,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
 }
