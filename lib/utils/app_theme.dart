0e72b905816572069ab5392611585d509ed91a71
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-11-29 13:02:40 +0530

    Update Dark Theme

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -2,37 +2,42 @@ import 'package:covid_19_tracker/data/data.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData themeData(bool isDarkTheme) {
-    return isDarkTheme ? darkTheme() : lightTheme();
-  }
-
   static ThemeData lightTheme() {
     return ThemeData(
       primaryColor: primaryBlack,
+      accentColor: primaryBlack,
       fontFamily: 'Circular',
       backgroundColor: Color(0xffF1F5FB),
       indicatorColor: Color(0xffCBDCF8),
       buttonColor: primaryBlack,
       highlightColor: Color(0xffFCE192),
-      hoverColor: Color(0xff4285F4),
       disabledColor: Colors.grey,
       textSelectionColor: Colors.black,
       cardColor: Colors.white,
       canvasColor: Colors.grey[50],
       brightness: Brightness.light,
       textTheme: TextTheme(
+        //Used in Main Headings like "WorldWide", "Statistics" etc.
         headline1: TextStyle(
           fontSize: 25,
           color: primaryBlack,
           fontWeight: FontWeight.bold,
         ),
+        //Used in some Bigger texts like "We stand together to fight with this".
         headline2: TextStyle(
           fontSize: 20,
           color: Colors.white,
           fontWeight: FontWeight.bold,
         ),
+        //Used in Custom Buttons like "Regional" , "India's Stats".
         headline3: TextStyle(
           fontSize: 16,
+          color: primaryBlack,
+          fontWeight: FontWeight.bold,
+        ),
+        //Used in Legend of PieChart Widget.
+        headline4: TextStyle(
+          fontSize: 15.0,
           color: primaryBlack,
           fontWeight: FontWeight.bold,
         ),
@@ -43,31 +48,40 @@ class AppTheme {
   static ThemeData darkTheme() {
     return ThemeData(
       primaryColor: primaryBlack,
-      backgroundColor: primaryBlack,
+      accentColor: Color(0xff4285F4),
+      backgroundColor: Color(0xff0E1D36),
       fontFamily: 'Circular',
       indicatorColor: Color(0xff0E1D36),
-      buttonColor: Colors.white70,
+      buttonColor: Colors.orange[100],
       highlightColor: Color(0xff372901),
-      hoverColor: Color(0xff3A3A3B),
       disabledColor: Colors.grey,
       textSelectionColor: Colors.white,
       cardColor: Color(0xFF151515),
       canvasColor: Colors.black,
       brightness: Brightness.dark,
       textTheme: TextTheme(
+        //Used in Main Headings like "WorldWide", "Statistics" etc.
         headline1: TextStyle(
           fontSize: 25,
           color: Colors.white,
           fontWeight: FontWeight.bold,
         ),
+        //Used in some Bigger texts like "We stand together to fight with this".
         headline2: TextStyle(
           fontSize: 20,
           color: primaryBlack,
           fontWeight: FontWeight.bold,
         ),
+        //Used in Custom Buttons like "Regional" , "India's Stats".
         headline3: TextStyle(
           fontSize: 16,
           color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        //Used in Legend of PieChart Widget.
+        headline4: TextStyle(
+          fontSize: 15.0,
+          color: primaryBlack,
           fontWeight: FontWeight.bold,
         ),
       ),
--- lib/utils/dark_theme_preference.dart
+++ lib/utils/dark_theme_preference.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class DarkThemePreference {
   static const THEME_STATUS = "CURRENT_THEME_STATUS";
 
-  void setDarkTheme(bool value) async {
+  Future<void> setDarkTheme(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool(THEME_STATUS, value);
   }
