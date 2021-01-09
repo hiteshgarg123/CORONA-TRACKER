c51d570fe0c640a87802eed58dd5aaaebfdc68d1
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2021-01-09 22:00:11 +0530

    New models and Hive TypeAdapters

---
+++ lib/models/statewiseData.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+part 'statewiseData.g.dart';
+
+@HiveType(typeId: 3)
+class StatewiseData {
+  @HiveField(0)
+  final String confirmed;
+  @HiveField(1)
+  final String active;
+  @HiveField(2)
+  final String deaths;
+  @HiveField(3)
+  final String recovered;
+  @HiveField(4)
+  final String lastupdatedtime;
+  @HiveField(5)
+  final String state;
+  @HiveField(6)
+  final String statecode;
+
+  StatewiseData({
+    @required this.active,
+    @required this.confirmed,
+    @required this.deaths,
+    @required this.recovered,
+    @required this.state,
+    @required this.statecode,
+    this.lastupdatedtime,
+  });
+
+  factory StatewiseData.fromJson(Map<String, dynamic> json) {
+    return StatewiseData(
+      active: json['active'],
+      confirmed: json['confirmed'],
+      deaths: json['deaths'],
+      lastupdatedtime: json['lastupdatedtime'],
+      recovered: json['recovered'],
+      state: json['state'],
+      statecode: json['statecode'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Confirmed: $confirmed, Active: $active, Deaths: $deaths, Recovered: $recovered';
+  }
+}
